114027f103c3dad168e7a05f744af843a0986cef
Pavel Ivanchik <ivanchik.pal@gmail.com>
2022-08-25 12:24:13 +0300

    C1
    completed add new user

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,7 +1,3 @@
-// Заполнение таблицы всеми юзерами из запроса
-// Method: get
-
-
 const url = 'http://localhost:8080/api/admin'
 const urlRoles = 'http://localhost:8080/api/admin/roles'
 const urlCurrentUser = 'http://localhost:8080/api/user'
@@ -20,75 +16,23 @@ const addNewUserRoleValue = document.getElementById('addNewUserRoles')
 
 const modalDefaultRoleValue = document.getElementById('modalDefaultRoles')
 
-
+// Заполнение таблицы всеми юзерами из запроса
+// Method: get
 loadUsers()
 loadCurrentUser()
 
-function loadUsers() {
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            data.forEach(user => {
-                usersTabRows.innerHTML += whiteRow(user)
-            })
-        })
-}
-
-function transformRole(roles) {
-    let listRole = ''
-    let delimiter = ''
-    roles.forEach(r => {
-        listRole += delimiter
-        listRole += r.role.substring(5);
-        delimiter = ' '
-    })
-    return listRole
-}
-
-function whiteRow(user) {
-    let tableBodyContent = ''
-    tableBodyContent += '<tr>'
-    tableBodyContent += '<td>' + user.id + '</td>'
-    tableBodyContent += '<td>' + user.firstName + '</td>'
-    tableBodyContent += '<td>' + user.lastName + '</td>'
-    tableBodyContent += '<td>' + user.age + '</td>'
-    tableBodyContent += '<td>' + user.username + '</td>'
-    tableBodyContent += '<td>' + transformRole(user.roles) + '</td>'
-    tableBodyContent += '<td><button type="button" class="btn btn-info" id="editUserId' + user.id + '" data-toggle="modal" data-target="#defaultModal"> Edit </button> </td>'
-    tableBodyContent += '<td><button type="button" class="btn btn-danger" id="deleteUserId' + user.id + '" data-toggle="modal" data-target="#defaultModal"> Delete </button> </td>'
-    tableBodyContent += '</tr>'
-    return tableBodyContent
-}
-
-
-
-function clearForm() {
-    addNewUserFirstNameValue.value = ''
-    addNewUserLastNameValue.value = ''
-    addNewUserAgeValue.value = ''
-    addNewUserEmailValue.value = ''
-    addNewUserPasswordValue.value = ''
-}
-
-// Лист значений ролей при создании нового пользователя из запроса к бд
+// Заполнение лист значений ролей в options select-ов из запроса к бд
 fetch(urlRoles)
     .then(res => res.json())
     .then(data => renderRolesList(data))
 
-const renderRolesList = (roles) => {
-    roles.forEach(roles => {
-        listOptionRoles += '<option value="' + roles.id + '">' + roles.role.toString().replace('ROLE_', '') + '</option>'
-    })
-    addNewUserRoleValue.innerHTML = listOptionRoles
-    modalDefaultRoleValue.innerHTML = listOptionRoles
-
-}
 
 // Добавление нового пользователя
 // Method: post
 
 let listOptionRoles = ''
 
+// События после отправки формы
 createNewUserForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -128,6 +72,18 @@ createNewUserForm.addEventListener('submit', (e) => {
         })
 })
 
+
+
+function loadUsers() {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(user => {
+                usersTabRows.innerHTML += whiteRow(user)
+            })
+        })
+}
+
 function loadCurrentUser() {
     fetch(urlCurrentUser)
         .then(res => res.json())
@@ -143,4 +99,46 @@ function loadCurrentUser() {
             row += '</tr>'
             currentUserRow.innerHTML += row
         })
+}
+
+function transformRole(roles) {
+    let listRole = ''
+    let delimiter = ''
+    roles.forEach(r => {
+        listRole += delimiter
+        listRole += r.role.substring(5);
+        delimiter = ' '
+    })
+    return listRole
+}
+
+function whiteRow(user) {
+    let tableBodyContent = ''
+    tableBodyContent += '<tr>'
+    tableBodyContent += '<td>' + user.id + '</td>'
+    tableBodyContent += '<td>' + user.firstName + '</td>'
+    tableBodyContent += '<td>' + user.lastName + '</td>'
+    tableBodyContent += '<td>' + user.age + '</td>'
+    tableBodyContent += '<td>' + user.username + '</td>'
+    tableBodyContent += '<td>' + transformRole(user.roles) + '</td>'
+    tableBodyContent += '<td><button type="button" class="btn btn-info" id="editUserId' + user.id + '" data-toggle="modal" data-target="#defaultModal"> Edit </button> </td>'
+    tableBodyContent += '<td><button type="button" class="btn btn-danger" id="deleteUserId' + user.id + '" data-toggle="modal" data-target="#defaultModal"> Delete </button> </td>'
+    tableBodyContent += '</tr>'
+    return tableBodyContent
+}
+
+function clearForm() {
+    addNewUserFirstNameValue.value = ''
+    addNewUserLastNameValue.value = ''
+    addNewUserAgeValue.value = ''
+    addNewUserEmailValue.value = ''
+    addNewUserPasswordValue.value = ''
+}
+
+const renderRolesList = (roles) => {
+    roles.forEach(roles => {
+        listOptionRoles += '<option value="' + roles.id + '">' + roles.role.toString().replace('ROLE_', '') + '</option>'
+    })
+    addNewUserRoleValue.innerHTML = listOptionRoles
+    modalDefaultRoleValue.innerHTML = listOptionRoles
 }
